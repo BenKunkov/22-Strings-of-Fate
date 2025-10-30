4851763a34591a025f8853bacb0cc3c11f8766f2
benku101 <bkunkov@gmail.com>
2025-10-30 10:48:08 -0500

    finally, finally done. also fixerd.

--- src/main/java/StringySpells.java
+++ src/main/java/StringySpells.java
@@ -22,7 +22,7 @@ public class StringySpells {
     }
 
     public static String flipper (String one, String two){
-        String first = one.substring(0);
+        String first = one.substring(1);
         String second = two.substring(0, 1);
         return second + first;
     }
